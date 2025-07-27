a83c75f88fb4193a90bcf06dd4064d6d6c89aca2
Sri Ram <sriramkm2004@gmail.com>
2025-07-27 22:31:04 +0530

    Switch to CoinMarketCap API

--- services/cryptoService.js
+++ services/cryptoService.js
@@ -1,5 +1,5 @@
+require('dotenv').config(); 
 const axios = require('axios');
-
 
 const priceCache = {
   BTC: { price: 0, lastUpdated: 0 },
@@ -8,27 +8,30 @@ const priceCache = {
 
 const fetchCryptoPrices = async () => {
   try {
-
-    const response = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd');
-
+    const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest', {
+      headers: {
+        'X-CMC_PRO_API_KEY': process.env.CMC_PRO_API_KEY,
+      },
+      params: {
+        symbol: 'BTC,ETH',
+        convert: 'USD'
+      }
+    });
+    
     const now = Date.now();
-    priceCache.BTC = { price: response.data.bitcoin.usd, lastUpdated: now };
-    priceCache.ETH = { price: response.data.ethereum.usd, lastUpdated: now };
-
-    console.log('Fetched new crypto prices:', priceCache);
+    priceCache.BTC = { price: response.data.data.BTC.quote.USD.price, lastUpdated: now };
+    priceCache.ETH = { price: response.data.data.ETH.quote.USD.price, lastUpdated: now };
+    
+    console.log('Fetched new crypto prices from CoinMarketCap:', priceCache);
 
   } catch (error) {
-    console.error('Error fetching crypto prices:', error.message);
-    
+    console.error('Error fetching crypto prices:', error.response ? error.response.data : error.message);
   }
 };
 
-
 fetchCryptoPrices();
 
-
-setInterval(fetchCryptoPrices, 20000);
-
+setInterval(fetchCryptoPrices, 60000); 
 
 const getPrice = (currency) => {
   return priceCache[currency.toUpperCase()]?.price || 0;
